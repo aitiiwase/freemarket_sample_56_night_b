e1c4eeb8b21c965c38a010ee090548b4f4b66662
adatu0527 <adatu0527@yahoo.ne.jp>
2019-10-16 23:05:48 +0900

    config/deploy/production.rb　修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -63,5 +63,5 @@
 server '18.179.104.79', user: 'ec2-user', roles: %w{app db web}
 
 
-# set :rails_env, "production"
-# set :unicorn_rack_env, "production"
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
